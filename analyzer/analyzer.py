5c6973dfc42dfbd092032766c55bacfe8b98c1a6
Daniele Di Bella <daniele.dibella99@gmail.com>
2024-11-26 19:44:54 +0100

    Small plotting commands added

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 vectorizer = TfidfVectorizer(stop_words='english')  # Stop words are ignored
 
 # Vectorize the corpus
-dataframe_path = file_path = r'C:\Users\danie\PycharmProjects\WikiSpark\crawler\tanc2_pubmed_abstracts_2024-11-26_16.24.csv'
+dataframe_path = file_path = r'C:\Users\danie\PycharmProjects\WikiSpark\analyzer\tanc2_pubmed_abstracts_2024-11-26_19.37.csv'
 df = pd.read_csv(file_path)
 corpus = df['Abstract']
 M = vectorizer.fit_transform(corpus)
@@ -18,10 +18,14 @@ tfidf_matrix = pd.DataFrame(M.toarray(), columns=words)
 
 # Extract the column of interest from he TF-IDF matrix and add it to the dataframe
 df["tfidf_tanc2"] = tfidf_matrix["tanc2"]
-df.to_csv(file_path, index=False)
+# print(df["tfidf_tanc2"])
+df.to_csv(file_path, index=True)
 
 # Small graphical representation
 plt.figure()
-df["tfidf_tanc2"].plot.hist(alpha=0.5)
+df["tfidf_tanc2"].plot(kind='bar', color='lightblue', alpha=0.7)
+plt.title('tanc2_TF-IDF')
+plt.xlabel('Paper index')
+plt.ylabel('TF-IDF')
 plt.show()
 
